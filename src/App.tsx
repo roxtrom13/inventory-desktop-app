aa051285ab02beab5bb3fafe132a6d8b6c67454f
Roy Palomino <roy100995@gmail.com>
2022-09-17 13:24:12 -0500

    refactor: rendering components based on loggin state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import type { Component } from "solid-js";
+import { Show } from "solid-js";
+import Store from "./store";
+
 import Default from "./layouts/default";
+import Blank from "./layouts/blank";
 
 const App: Component = () => {
+
+  const [store] = Store;
+
   return (
-    <>
+    <Show when={!!store.user}
+      fallback={<Blank />}
+    >
       <Default />
-    </>
+    </Show>
   );
 };
 
