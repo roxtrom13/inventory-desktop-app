074d828c1e1789766852b6ff1216c99c9594cb38
Roy Palomino <roy100995@gmail.com>
2022-09-19 13:54:36 -0500

    minimal changes

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,6 +1,6 @@
 export default function Topbar() {
   return (
-    <div class="sticky p-2 h-16 top-0 ml-24 md:ml-60 z-30 w-[calc(100% - 6rem)] md:w-[calc(100% - 15rem)] bg-cyan-600">
+    <div class="sticky p-2 h-16 top-0 ml-24 md:ml-60 z-30 w-[calc(100% - 6rem)] md:w-[calc(100% - 15rem)] bg-cyan-500">
       <p class="text-white text-2xl">Topbar content here!</p>
     </div>
   )
