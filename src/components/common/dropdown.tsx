80231d75350988f14209c25eaa0fd6355b0d64a7
Roy Palomino <roy100995@gmail.com>
2022-09-27 11:30:41 -0500

    refactor: update dropdown as a avatar component

--- src/components/common/dropdown.tsx
+++ src/components/common/dropdown.tsx
@@ -1,50 +1,120 @@
-import { createStore } from "solid-js/store";
+import { children } from "solid-js";
+import { mergeProps } from "solid-js";
 
 import { Icon } from "solid-heroicons";
-import { chevronDown, chevronUp } from "solid-heroicons/solid-mini";
+import { chevronDown, chevronUp, arrowLeftOnRectangle } from "solid-heroicons/solid-mini";
+import {
+  Popover,
+  PopoverButton,
+  PopoverPanel,
+  Transition,
+  Menu,
+  MenuItem,
+} from "solid-headless";
+
+enum DropdownPosition {
+  "left",
+  "right",
+}
+
+interface DropdownProps {
+  children: any;
+  position?: DropdownPosition;
+  class?: string;
+  onClick?: Function | Promise<any>;
+  disabled?: boolean;
+}
+
+function print() {
+  console.log("selected!!");
+}
+
+function endSession() {
+
+}
+
+function classNames(...classes: (string | boolean | undefined)[]): string {
+  return classes.filter(Boolean).join(" ");
+}
+
+// function Separator() {
+//   return (
+//     <div class="flex items-center" aria-hidden="true">
+//       <div class="w-full border-t border-gray-200" />
+//     </div>
+//   );
+// }
 
 export default function Dropdown() {
-  const [state, setState] = createStore({
-    open: false,
-  });
+  // const props = mergeProps(
+  //   {
+  //     position: DropdownPosition.left,
+  //     disabled: false,
+  //   },
+  //   properties
+  // );
+
+  // const c = children(() => props.children);
 
   return (
-    <div
-      onClick={() => setState("open", !state.open)}
-      class="relative cursor-pointer"
-    >
-      <div class="flex items-center">
-        <button class="bg-red-400">hola</button>{" "}
-        {/* this is the child and should be replaced */}
-        <Icon
-          class="ml-1"
-          path={state.open ? chevronUp : chevronDown}
-          height="20px"
-          color="white"
-        />
-      </div>
-      <div hidden={!state.open} class="absolute z-10 w-56 mt-4 origin-top-right bg-white border border-gray-100 rounded-md shadow-lg">
-        <div class="p-2">
-          <a
-            href="#"
-            class="block px-4 py-2 text-sm text-gray-500 rounded-lg hover:bg-gray-50 hover:text-gray-700"
+    <Popover defaultOpen={false} class="relative">
+      {({ isOpen, setOpen }) => (
+        <>
+          <div class="flex flex-row items-center">
+            <PopoverButton>
+              {/* THIS IS THE CHILD */}
+              <img
+                class="inline-block h-12 w-12 rounded-full ring-2 ring-white"
+                src="https://avatars.githubusercontent.com/u/30743104?v=4"
+              ></img>
+              {/* END OF THE CHILD */}
+            </PopoverButton>
+            <Icon
+              path={isOpen() ? chevronUp : chevronDown}
+              class={classNames(
+                isOpen() && "text-opacity-70",
+                "ml-2 h-6 text-white group-hover:text-opacity-80 transition ease-in-out duration-150"
+              )}
+            ></Icon>
+          </div>
+          <Transition
+            show={isOpen()}
+            enter="transition duration-200"
+            enterFrom="opacity-0 -translate-y-1 scale-50"
+            enterTo="opacity-100 translate-y-0 scale-100"
+            leave="transition duration-150"
+            leaveFrom="opacity-100 translate-y-0 scale-100"
+            leaveTo="opacity-0 -translate-y-1 scale-50"
           >
-            ReactJS Dropdown 1
-          </a>
-          <a
-            href="#"
-            class="block px-4 py-2 text-sm text-gray-500 rounded-lg hover:bg-gray-50 hover:text-gray-700"
-          >
-            ReactJS Dropdown 2
-          </a>
-          <a
-            href="#"
-            class="block px-4 py-2 text-sm text-gray-500 rounded-lg hover:bg-gray-50 hover:text-gray-700"
-          >
-            ReactJS Dropdown 3
-          </a>
-        </div>
-      </div>
-    </div>
+            <PopoverPanel
+              unmount={false}
+              class="absolute z-10 px-4 mt-3 transform -translate-x-2/3 left-1/2 sm:px-0 lg:max-w-3xl"
+            >
+              <Menu class="overflow-hidden w-64 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white flex flex-col space-y-1 p-1">
+                <MenuItem
+                  as="button"
+                  class="text-sm p-1 text-left rounded hover:bg-purple-600 hover:text-white focus:outline-none focus:bg-purple-600 focus:text-white"
+                >
+                  Open Link in New Tab
+                </MenuItem>
+                <MenuItem
+                  as="button"
+                  class="text-sm p-1 text-left rounded hover:bg-purple-600 hover:text-white focus:outline-none focus:bg-purple-600 focus:text-white"
+                >
+                  hola
+                </MenuItem>
+                <MenuItem
+                  as="button"
+                  class="flex flex-row text-sm p-1 text-left rounded hover:bg-purple-600 hover:text-white focus:outline-none focus:bg-purple-600 focus:text-white"
+                >
+                  <Icon height="20px" path={arrowLeftOnRectangle} />
+                  Salir
+                </MenuItem>
+              </Menu>
+            </PopoverPanel>
+          </Transition>
+        </>
+      )}
+    </Popover>
   );
 }
