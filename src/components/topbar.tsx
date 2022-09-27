23b904309af37d8f724d55652a8d43262275349c
Roy Palomino <roy100995@gmail.com>
2022-09-27 11:31:09 -0500

    feat: reduce logic and delegate to dropdown component

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,7 +1,17 @@
+// import { createStore } from "solid-js/store";
+// import Store from "../store";
+import Dropdown from "../components/common/dropdown";
+
+var src = "";
+
 export default function Topbar() {
+
+
+
   return (
-    <div class="sticky p-2 h-16 top-0 ml-24 md:ml-60 z-30 w-[calc(100% - 6rem)] md:w-[calc(100% - 15rem)] bg-cyan-500">
-      <p class="text-white text-2xl">Topbar content here!</p>
+    <div class="sticky py-2 px-8 h-16 top-0 ml-24 md:ml-60 z-30 w-[calc(100% - 6rem)] md:w-[calc(100% - 15rem)] bg-cyan-500 flex items-center justify-between">
+      <p class="text-white text-2xl">&#123; title &#125;</p>
+      <Dropdown></Dropdown>
     </div>
-  )
+  );
 }
