5a81508bfd593b82b44ff35140ef0af87ff9f685
Roy Palomino <roy100995@gmail.com>
2022-09-27 11:34:12 -0500

    refactor: change code for button by customized button component

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -129,41 +129,14 @@ export default function Login() {
                   </div>
                   <p class="error-form-message">{passwordError()}</p>
                 </div>
-                <button
-                  class="btn btn-primary mt-6"
-                  classList={{ "cursor-not-allowed": state.loading }}
-                  type="submit"
-                  disabled={state.loading}
+                <ButtonComponent
+                  class="btn-primary"
+                  loading={state.loading}
+                  loadingText="Espere"
+                  onClick={handleLogin()}
                 >
-                  <Show
-                    when={state.loading}
-                    fallback={<span>Iniciar sesión</span>}
-                  >
-                    <div class="flex justify-center items-center">
-                      <svg
-                        class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                      >
-                        <circle
-                          class="opacity-25"
-                          cx="12"
-                          cy="12"
-                          r="10"
-                          stroke="currentColor"
-                          stroke-width="4"
-                        ></circle>
-                        <path
-                          class="opacity-75"
-                          fill="currentColor"
-                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                        ></path>
-                      </svg>
-                      Cargando ...
-                    </div>
-                  </Show>
-                </button>
+                  Iniciar sesión
+                </ButtonComponent>
               </div>
             );
           }}
