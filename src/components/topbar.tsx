3c70893087912a5043f1c52760aa36479e3ed3ea
Roy Palomino <roy100995@gmail.com>
2022-10-03 11:37:00 -0500

    feat: add custom styles to dropdown items

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,17 +1,11 @@
-// import { createStore } from "solid-js/store";
-// import Store from "../store";
-import Dropdown from "../components/common/dropdown";
-
-var src = "";
+import DropdownAvatar from "../components/common/dropdownAvatar";
 
 export default function Topbar() {
 
-
-
   return (
     <div class="sticky py-2 px-8 h-16 top-0 ml-24 md:ml-60 z-30 w-[calc(100% - 6rem)] md:w-[calc(100% - 15rem)] bg-cyan-500 flex items-center justify-between">
-      <p class="text-white text-2xl">&#123; title &#125;</p>
-      <Dropdown></Dropdown>
+      <p class="text-white text-2xl"></p>
+      <DropdownAvatar></DropdownAvatar>
     </div>
   );
 }
