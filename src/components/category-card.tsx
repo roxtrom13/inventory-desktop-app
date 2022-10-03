3e93f13664825ff88857192cd113e1a213b81c7a
Roy Palomino <roy100995@gmail.com>
2022-10-03 18:12:48 -0500

    feat: add styles and dump content

--- src/components/category-card.tsx
+++ src/components/category-card.tsx
@@ -1,7 +1,11 @@
 export default function CategoryCard() { 
   return (
-    <div>
-      I'm a category component
+    <div class="flex flex-row bg-white my-3 p-2 shadow-sm rounded-md max-w-xs cursor-pointer hover:shadow-lg transition-all duration-200">
+      <div class="flex-none h-20 w-20 bg-gray-500 mr-2"></div>
+      <div>
+        <h3 class="text-xl font-semibold">Bajo</h3>
+        <p class="text-gray-700 text-sm">Voluptas ad reprehenderit dolores quidem asperiores quis et. Itaque a ea nemo vel non.</p>
+      </div>
     </div>
   )
 }
