ef052d6e96b5b9b10506d8b3c1a30362b2414c7a
Roy Palomino <roy100995@gmail.com>
2022-10-03 18:13:40 -0500

    refactor: remove route indicator and change layout to scrollable content

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,22 +1,22 @@
 import { lazy } from "solid-js";
 import SideBar from "../components/sidebar";
 import Topbar from "../components/topbar";
-import { Routes, Route, useLocation } from "solid-app-router";
+import { Routes, Route } from "solid-app-router";
 
 const Home = lazy(() => import("../pages/home"));
 const Sales = lazy(() => import("../pages/sales"));
+const Categories = lazy(() => import("../pages/categories"));
 
 export default function Default() {
-  const location = useLocation();
   return (
-    <div class="relative min-h-screen w-full">
+    <div class="relative min-h-screen w-full bg-white">
       <SideBar />
       <Topbar />
-      <section class="p-2 relative min-h-screen left-24 md:left-60 w-[calc(100%-6rem)] md:w-[calc(100%-15rem)] z-10">
-      <h1>{ JSON.stringify(location) }</h1>
+      <section class="p-2 relative max-h-screen left-24 md:left-60 w-[calc(100%-6rem)] md:w-[calc(100%-15rem)] z-10 overflow-y-hidden">
         <Routes>
           <Route path="/" component={Home} />
           <Route path="/sales" component={Sales} />
+          <Route path="/inventory/categories" component={Categories} />
         </Routes>
       </section>
     </div>
