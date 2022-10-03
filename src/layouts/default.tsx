b6674657c2abbafb5ea65932856b27304c6226a0
Roy Palomino <roy100995@gmail.com>
2022-10-03 11:37:44 -0500

    refactor: add temp location indicator

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,17 +1,19 @@
 import { lazy } from "solid-js";
 import SideBar from "../components/sidebar";
 import Topbar from "../components/topbar";
-import { Routes, Route } from "solid-app-router";
+import { Routes, Route, useLocation } from "solid-app-router";
 
 const Home = lazy(() => import("../pages/home"));
 const Sales = lazy(() => import("../pages/sales"));
 
 export default function Default() {
+  const location = useLocation();
   return (
     <div class="relative min-h-screen w-full">
       <SideBar />
       <Topbar />
       <section class="p-2 relative min-h-screen left-24 md:left-60 w-[calc(100%-6rem)] md:w-[calc(100%-15rem)] z-10">
+      <h1>{ JSON.stringify(location) }</h1>
         <Routes>
           <Route path="/" component={Home} />
           <Route path="/sales" component={Sales} />
