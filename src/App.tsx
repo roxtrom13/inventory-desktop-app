8f800a3a7f4b8dddc39eacfa50e93a7cea96d081
Roy Palomino <roy100995@gmail.com>
2022-10-03 11:33:20 -0500

    feat: add function to check wether user is loggedin or not

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,26 @@ import Store from "./store";
 import Default from "./layouts/default";
 import Blank from "./layouts/blank";
 
+const image = new File(["blob"], "ga");
+console.log(image);
+
 const App: Component = () => {
 
-  const [store] = Store;
+  const [store, setStore] = Store;
+
+  function loggedInUser() {
+    let storageUser = localStorage.getItem("user");
+    if (!!store.user) return true;
+    if (!!storageUser) {
+      const user = JSON.parse(storageUser);
+      setStore("user", user);
+      return true;
+    }
+    return false;
+  }
 
   return (
-    <Show when={!store.user}
+    <Show when={loggedInUser()}
       fallback={<Blank />}
     >
       <Default />
